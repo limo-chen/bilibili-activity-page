c2434c0d5f7af333d4837dba36dedcc3e43ab5ef
Calista <2320035651@qq.com>
2024-03-02 16:55:58 +0800

    实现高亮并且加动画丝滑切换

--- src/sections/SecondSection/index.tsx
+++ src/sections/SecondSection/index.tsx
@@ -1,18 +1,48 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import food from "../../assets/food.jpg";
 import life from "../../assets/life.jpg";
 import movie from "../../assets/movie.png";
 import cartoon from "../../assets/cartoon.jpg";
 import styles from "./styles.module.scss";
+import classNames from "classnames";
+
+const tabs = [
+  {
+    key: "cartoon",
+    title: "CARTOON",
+  },
+  {
+    key: "food",
+    title: "FOOD",
+  },
+  {
+    key: "movie",
+    title: "MOVIE",
+  },
+  {
+    key: "life",
+    title: "LIFE",
+  },
+];
 
 const SecondSection: FC = () => {
+  const [activeTab, setActiveTab] = useState<string>("cartoon");
+
   return (
     <div className={styles.secondSection}>
       <ul>
-        <li>CARTOON</li>
-        <li>FOOD</li>
-        <li>MOVIE</li>
-        <li>LIFE</li>
+        {tabs.map((tab) => {
+          return (
+            <li key={tab.key} onClick={() => setActiveTab(tab.key)}>
+              <span>{tab.title}</span>
+              <span
+                className={classNames(styles.line, {
+                  [styles.visible]: activeTab === tab.key,
+                })}
+              ></span>
+            </li>
+          );
+        })}
       </ul>
 
       <div>
